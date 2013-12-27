968f3126abf3fab386043971423ab4a4e706a945
Robert Chu <robert@wibidata.com>
2013-12-26 17:55:33 -0800

    Updated.

--- xmonad.hs
+++ xmonad.hs
@@ -1,11 +1,14 @@
 import XMonad
+import XMonad.Actions.FindEmptyWorkspace
 import XMonad.Actions.Volume
 import XMonad.Config.Gnome
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
 import XMonad.Util.EZConfig
 
 -- Custom hotkeys.
+customKeysP :: [(String, X ())]
 customKeysP =
     [
       -- Application shortcuts.
@@ -15,23 +18,58 @@ customKeysP =
       ("M-a", spawn "hipchat"),
       ("M-v", spawn "spotify"),
       ("M-z", spawn "gvim ~/.xmonad/xmonad.hs"),
+      ("M-i", spawn "/home/robert/lib/idea/bin/idea.sh"),
 
       -- Volume shortcuts. These require that 'xmonad-extras' is installed.
       ("M-<Up>", raiseVolume 3 >> return ()),
-      ("M-<Down>", lowerVolume 3 >> return ())
+      ("M-<Down>", lowerVolume 3 >> return ()),
+
+      -- Find empty workspace shortcuts.
+      ("M-y", viewEmptyWorkspace),
+      ("M-S-y", tagToEmptyWorkspace),
+
+      -- Launcher shortcut.
+      ("M-;", spawn "/usr/bin/dmenu_run")
     ]
 
 -- Key unbindings.
 customUnkeysP =
     [
-      -- default
-      --   run program launcher.
-      -- reason
-      --   kupfer handles its own keys.
+      -- Default
+      --   Run program launcher.
+      -- Reason
+      --   This hotkey has a race condition with the monitor switch gnome keybinding.
       "M-p"
     ]
 
+customWorkspaces =
+    [
+      "1:edit",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9:web"
+    ]
+
+customManageHook = composeAll
+    [
+      -- Send browser windows to the web workspace.
+      appName =? "google-chrome" --> doShift "9:web",
+
+      -- Send IDE to the edit workspace.
+      className =? "jetbrains-idea-ce" --> doShift "1:edit",
+      className =? "jetbrains-idea" --> doShift "1:edit",
+
+      -- Float xmessage windows.
+      className =? "Xmessage" --> doFloat
+    ]
+
 -- Custom settings overriding defaults from 'gnomeConfig'.
+-- customConfig = ewmh gnomeConfig
 customConfig = gnomeConfig
     {
       -- Switch from using <Ctrl> as the window manager key to <Super> (also known as the
@@ -46,8 +84,15 @@ customConfig = gnomeConfig
       handleEventHook = fullscreenEventHook,
 
       -- Fix for java swing applications.
-      startupHook = setWMName "LG3D"
+      startupHook = setWMName "LG3D",
+
+      -- Use custom workspaces.
+      workspaces = customWorkspaces,
+
+      -- Create certain programs in specific workspaces.
+      manageHook = manageDocks <+> customManageHook <+> manageHook gnomeConfig
     }
 
 -- Start XMonad.
+main :: IO ()
 main = (xmonad (additionalKeysP (removeKeysP customConfig customUnkeysP) customKeysP))
